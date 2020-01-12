8d81485e972c5074adde971e407b5509f5152d89
Anna Thammakane <anna.thammakane@gmail.com>
2020-01-12 15:50:13 -0600

    linked to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "",
+  user: "Anna",
+  password: "Password123",
   database: "burgers_db"
 });
 
--- server.js
+++ server.js
@@ -19,6 +19,12 @@ var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
 app.listen(PORT, function() {
   console.log("Listening on port:%s", PORT);
 });
